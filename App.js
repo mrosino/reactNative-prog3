a4c9ae07ff2a44a1db85ef7ba3009ae93828f7cd
Adela <69322992+addbuye@users.noreply.github.com>
2021-11-03 12:04:19 -0300

    primeras pantallas

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Card from './src/components/Card';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Card />
     </View>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createDrawerNavigator } from '@react-navigation/drawer'
+import Home from '../screens/home';
+import Login from '../screens/login';
+import Register from '../screens/register';
+import Profile from '../screens/miPerfil';
+import PostForm from '../screens/postForm';
+import { auth } from '../firebase/config';
+const Drawer = createDrawerNavigator();
+class Menu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedin: false,
+            userData: {}
+        }
+    }
+    componentDidMount(){
+        auth.onAuthStateChanged(
+            user => {
+                if (user){
+                    this.setState({
+                        loggedin: true,
+                        userData: user,
+                    })    
+                }
+            }
+        )
+    }
+    register(email, pass){
+        auth.createUserWithEmailAndPassword(email, pass)
+            .then(()=>{
+                console.log('Registrado ok');
+            })
+            .catch( error => {
+                console.log(error);
+            })
+    }
+    login(email, pass){
+        auth.signInWithEmailAndPassword(email, pass)
+            .then( user => {
+                console.log('Login Ok');
+                this.setState({
+                    loggedin: true,
+                    userData: user
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    logout(){
+        auth.signoOut()
+        .then( 
+            this.setState({
+                loggedin: false,
+            })
+        )
+        .catch(e => console.log(e))
+    }
+    render(){
+        return(
+            <NavigationContainer>
+                { this.state.loggedin === false ?
+                <Drawer.Navigator>
+                    <Drawer.Screen name="Login" component={ ()=> <Login login={(email, pass)=>this.login(email, pass) } />}/>
+                    <Drawer.Screen name="Registro" component={ ()=> <Register register={(email, pass)=>this.register(email, pass)} />}/>
+                </Drawer.Navigator>:
+                <Drawer.Navigator>
+                    <Drawer.Screen name="Home" component={ ()=> <Home />}/>
+                    <Drawer.Screen name="Nuevo Post" component={ (drawerProps)=> <PostForm drawerProps={drawerProps}/>}/>
+                    <Drawer.Screen name="Mi Perfil" component={ ()=> <Profile userData={this.state.userData} logout={()=>this.logout()} />}/>
+                </Drawer.Navigator>
+                }
+            </NavigationContainer>
+        )
+    }
+}
+export default Menu;
